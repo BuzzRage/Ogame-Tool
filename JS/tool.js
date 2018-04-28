33fba559ab1c20b2f3c36facc56d26fc2c82c3ee
Buzzrage <marius95@live.fr>
2018-04-28 12:05:55 -0400

    Adding get_resources(), begginning menu and first feature

--- JS/tool.js
+++ JS/tool.js
@@ -2,16 +2,50 @@ var curr_resources = [];
 
 function ogame_tool(){
 	var continuer = 1;
-	
-	curr_resources.push(document.getElementById("resources_metal").innerHTML);
-	curr_resources.push(document.getElementById("resources_crystal").innerHTML);
-	curr_resources.push(document.getElementById("resources_deuterium").innerHTML);
-	curr_resources.push(document.getElementById("resources_darkmatter").innerHTML);
-	curr_resources.push(document.getElementById("resources_energy").innerHTML);
-	
-	
+		
 	console.log("------- Ogame Tool Begin -----");
 	console.log("Ce programme va vous assister à la production de vos batiments en fonction de vos ressources.");
 	console.log("Que voulez-vous faire ?");
+	
+	var ans = prompt("
+	1. Savoir combien de ressources seront produites dans X temps ? \n
+	2. Savoir dans combien de temps les ressources requises pour produire un batiments seront acquises ? \n
+	");
+	
+	switch(ans){
+		case 1:
+			how_much_resources_in_time();
+			break;
+		case 2:
+			break;
+		default:
+			break;
+	}
+	
 	console.log("------- Ogame Tool End -------");
+}
+
+function get_resources(){
+	curr_resources = [];
+	curr_resources.push(document.getElementById("resources_metal").innerHTML);
+	curr_resources.push(document.getElementById("resources_crystal").innerHTML);
+	curr_resources.push(document.getElementById("resources_deuterium").innerHTML);
+	//curr_resources.push(document.getElementById("resources_darkmatter").innerHTML);
+	curr_resources.push(document.getElementById("resources_energy").innerHTML);
+}
+
+function how_much_resources_in_time(){
+	var time = prompt("Combien de temps voulez vous évaluez ?");
+	time = format_time(time);
+	var results = [0,0,0,0];
+	for(var i=0;i<4;i++){
+		results.push(prod[i]*time + curr_resources[i]);
+	}
+	});
+	
+	console.
+}
+
+function format_time(t){
+	
 }
